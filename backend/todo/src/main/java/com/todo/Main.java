156418c32386d86afc3c78944298629e7760c480
sarerama <smirkovic995@gmail.com>
2020-01-20 19:50:32 +0100

    Bugfix spring boot application

--- backend/todo/src/main/java/com/todo/Main.java
+++ backend/todo/src/main/java/com/todo/Main.java
@@ -3,11 +3,9 @@ package com.todo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-@Configuration
 public class Main {
 	public static void main(String[] args) {
 		SpringApplication.run(Main.class, args);
